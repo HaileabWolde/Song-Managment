f975952f1464c97359b7417bf032caeb75852706
Haileab <womahaileab1428@gmail.com>
2024-02-04 22:23:38 +0300

    added the form table

--- client/src/Forms.tsx
+++ client/src/Forms.tsx
@@ -1,7 +1,30 @@
 const Forms = ()=> {
     return (
-        <div className="w-[25%] bg-white">
-            <h1>Hello Forms </h1>
+        <div className="w-[30%] bg-white p-4 flex flex-col gap-4">
+            <h1 className="text-xl font-semibold text-center">Creating A Song </h1>
+            <form className="w-[90%] mx-auto flex flex-col gap-4">
+                <input
+                type="text"
+                placeholder="Add Song Name"
+                className="p-5 bg-white rounded-lg shadow-xl w-full border text-sm"
+                />
+                 <input
+                type="text"
+                placeholder="Add The Name of The Artist"
+                className="p-5 bg-white rounded-lg shadow-xl w-full border text-sm"
+                />
+                 <input
+                type="text"
+                placeholder="Add The Name of The Album"
+                className="p-5 bg-white rounded-lg shadow-xl w-full border text-sm"
+                />
+                 <input
+                type="text"
+                placeholder="Add Genre Type"
+                className="p-5 bg-white rounded-lg shadow-xl w-full border text-sm"
+                />
+
+            </form>
         </div>
     )
 }
